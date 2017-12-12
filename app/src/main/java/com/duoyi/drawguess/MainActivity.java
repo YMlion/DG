fe6bef52cecbdf633b80737ee240b5014d36940d
YMlion <lm_yao@163.com>
2017-12-12 17:43:10 +0800

    add some libs

--- app/src/main/java/com/duoyi/drawguess/MainActivity.java
+++ app/src/main/java/com/duoyi/drawguess/MainActivity.java
@@ -1,7 +1,7 @@
 package com.duoyi.drawguess;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
