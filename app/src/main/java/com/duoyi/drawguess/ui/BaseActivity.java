76e030f17254051dc0ffbe1dc61ef1682fb617f8
YMlion <lm_yao@163.com>
2017-12-15 10:52:42 +0800

    base activity init

--- app/src/main/java/com/duoyi/drawguess/ui/BaseActivity.java
+++ app/src/main/java/com/duoyi/drawguess/ui/BaseActivity.java
@@ -3,17 +3,76 @@ package com.duoyi.drawguess.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import com.duoyi.drawguess.util.DLog;
 
 /**
+ * activity基类，公共方法及生命周期管理
  * Created by YMlion on 2017/12/14.
  */
 
-public class BaseActivity extends AppCompatActivity {
+public class BaseActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         DLog.d("activity onCreate");
+    }
+
+    @Override public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        init();
+    }
+
+    private void init() {
+        initView();
+        initData();
+    }
+
+    /**
+     * 数据初始化
+     */
+    protected void initData() {
+
+    }
+
+    /**
+     * view初始化
+     */
+    protected void initView() {
+
+    }
+
+    protected View fv(int resId) {
+        return findViewById(resId);
+    }
+
+    /**
+     * 设置View点击事件
+     *
+     * @param resId view id
+     * @return the view
+     */
+    protected View setOnClickListener(int resId) {
+        View v = fv(resId);
+        v.setOnClickListener(this);
+        return v;
+    }
+
+    /**
+     * 设置View点击事件
+     *
+     * @param resId view id
+     * @param listener 点击回调
+     * @return the view
+     */
+    protected View setOnClickListener(int resId, View.OnClickListener listener) {
+        View v = fv(resId);
+        v.setOnClickListener(listener);
+        return v;
+    }
+
+    @Override public void onClick(View v) {
+
     }
 
     @Override protected void onStart() {
--- app/src/main/java/com/duoyi/drawguess/ui/GameLobbyActivity.java
+++ app/src/main/java/com/duoyi/drawguess/ui/GameLobbyActivity.java
@@ -1,12 +1,35 @@
 package com.duoyi.drawguess.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import com.duoyi.drawguess.R;
 
+/**
+ * 游戏大厅界面
+ */
 public class GameLobbyActivity extends BaseActivity {
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_lobby);
+    }
+
+    @Override protected void initView() {
+        super.initView();
+        setOnClickListener(R.id.btn_lobby_dg_open);
+    }
+
+    @Override public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_lobby_dg_open:
+                playDG();
+                break;
+        }
+    }
+
+    private void playDG() {
+        Intent intent = new Intent(this, DrawGuessActivity.class);
+        startActivity(intent);
     }
 }
